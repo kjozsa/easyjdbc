46a64cd8ca27d3b09b1e634976be3f44364c2fac
Kristof Jozsa <kristof.jozsa@gmail.com>
2011-06-19 12:16:34 +0200

    update method

--- src/freeside/Client.scala
+++ src/freeside/Client.scala
@@ -29,12 +29,18 @@ object Client extends App {
     println(results.getString(1))
   }
 
-  // wtf1
+  // simple select
   sqlhelper.query("select 1 from dual") { _.getString(1) }
 
-  val name, date = ("Joe", new java.util.Date())
-  //  val rs = sqlhelper.query3("select * from person where name = ? and born > ?") << (name, date)
+  // select for one 
+  val zip: Option[Tuple2[String, Boolean]] = sqlhelper.queryOne("select city, zip from person where name = ?", "Joe") { rs => (rs.getString(2), rs.getBoolean(3)) }
+  val client: Option[Boolean] = sqlhelper.queryOne("select is_client from person where name = ?", "Joe") { _.getBoolean(1) }
 
+  // with parameters
+  val name, date = ("Joe", new java.util.Date())
   sqlhelper.query("select * from person where name = ? and born > ?", name, date) { _.getString(1) }
+
+  // update
+  sqlhelper.update("update person set divorced = ?", true)
 
 }
--- src/freeside/SQLHelper.scala
+++ src/freeside/SQLHelper.scala
@@ -19,41 +19,32 @@ class SQLHelper(
   println("using factory: " + factory + ", errorhandler: " + errorHandler)
 
   /** execute 'something' using a new or wrapped connection */
-  def execute[T](sqlBlock: Connection => T): T = {
+  def execute[T](executeBlock: Connection => T): T = {
+    val connection = factory.connection
     try {
-      sqlBlock(factory.connection)
+      executeBlock(connection)
     } catch {
       case e =>
-        rollback(factory.connection)
+        rollback(connection)
         throw errorHandler.prepare(e)
     }
   }
 
+  private def rollback(connection: Connection) {
+    try {
+      if (!connection.getAutoCommit) connection.rollback
+    } catch {
+      case e => // silent 
+    }
+  }
+
+  /** execute an sql query and process a list of results by records */
   def query[T](sql: String, params: Any*)(resultProcessor: ResultSet => T): List[T] = {
     execute { connection =>
-      val statement = connection.prepareStatement(sql);
-
-      params.view.zipWithIndex foreach {
-        case (param, index) =>
-          val position = 1 + index
-
-          param match {
-            case param: Boolean => statement.setBoolean(position, param)
-            case param: Byte => statement.setByte(position, param)
-            case param: Int => statement.setInt(position, param)
-            case param: Long => statement.setLong(position, param)
-            case param: Float => statement.setFloat(position, param)
-            case param: Double => statement.setDouble(position, param)
-            case param: Timestamp => statement.setTimestamp(position, param)
-            //          case param: BigDecimal => statement.setBigDecimal(position, param) @TODO turn bigdecimal to java 
-            case param: String => statement.setString(position, param)
-            case param => throw new UnsupportedOperationException("Unsupported parameter type of " + param)
-          }
-      }
-
+      val statement = prepareStatement(connection, sql, params)
       val results = statement.executeQuery;
 
-      var processed = List[T]()
+      var processed: List[T] = Nil
       while (results.next()) {
         processed = resultProcessor(results) :: processed
       }
@@ -61,14 +52,46 @@ class SQLHelper(
     }
   }
 
-  def rollback(connection: Connection) {
-    if (!connection.getAutoCommit) connection.rollback
+  /** query for 0 or 1 record */
+  def queryOne[T](sql: String, params: Any*)(resultProcessor: ResultSet => T): Option[T] = {
+    val results = query(sql, params)(resultProcessor)
+    results.length match {
+      case 0 => None
+      case 1 => Some(results.head)
+      case x => throw new IllegalStateException("More than one records found")
+    }
   }
-}
 
-class DefaultErrorHandler extends SQLHelper.ErrorHandler {
-  override def prepare(e: Throwable) = {
-    new RuntimeException(e)
+  /** execute an update */
+  def update(sql: String, params: Any*) {
+    execute { connection =>
+      val statement = prepareStatement(connection, sql, params)
+      statement executeQuery
+    }
+  }
+
+  /** set the parameters on the prepared statement */
+  private def prepareStatement(connection: Connection, sql: String, params: Any*) = {
+    val statement = connection.prepareStatement(sql);
+
+    params.view.zipWithIndex foreach {
+      case (param, index) =>
+        val position = 1 + index
+
+        param match {
+          case param: Boolean => statement.setBoolean(position, param)
+          case param: Byte => statement.setByte(position, param)
+          case param: Int => statement.setInt(position, param)
+          case param: Long => statement.setLong(position, param)
+          case param: Float => statement.setFloat(position, param)
+          case param: Double => statement.setDouble(position, param)
+          case param: Timestamp => statement.setTimestamp(position, param)
+          //          case param: BigDecimal => statement.setBigDecimal(position, param) @TODO turn bigdecimal to java 
+          case param: String => statement.setString(position, param)
+          case param => throw new UnsupportedOperationException("Unsupported parameter type of " + param)
+        }
+    }
+    statement
   }
 }
 
@@ -79,5 +102,10 @@ object SQLHelper {
   trait ErrorHandler {
     def prepare(e: Throwable): Throwable
   }
-}
 
+  class DefaultErrorHandler extends SQLHelper.ErrorHandler {
+    override def prepare(e: Throwable) = {
+      new RuntimeException(e)
+    }
+  }
+}
