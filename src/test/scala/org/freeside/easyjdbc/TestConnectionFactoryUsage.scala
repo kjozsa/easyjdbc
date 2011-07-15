6604faa478ca253c1b13a611cc8e39a992efdc2f
Kristof Jozsa <kristof.jozsa@gmail.com>
2011-07-15 14:19:54 +0200

    fix test logic

--- src/test/scala/org/freeside/easyjdbc/TestConnectionFactoryUsage.scala
+++ src/test/scala/org/freeside/easyjdbc/TestConnectionFactoryUsage.scala
@@ -2,10 +2,12 @@ package org.freeside.easyjdbc
 
 import java.sql.Connection
 import org.mockito.Mockito._
+import org.mockito.Matchers._
 import org.scalatest.{ BeforeAndAfter, FunSuite }
 import org.scalatest.mock.MockitoSugar
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
+import java.sql.PreparedStatement
 
 /**
  * @author kjozsa
@@ -18,21 +20,41 @@ class TestConnectionFactoryUsage extends FunSuite with MockitoSugar with BeforeA
 
   before({
     count = 0
+
     EasyJDBC.connectionFactory = { () =>
       count += 1
-      mock[Connection]
+      val connection = mock[Connection]
+      val statement = mock[PreparedStatement]
+      when(connection.prepareStatement(any())).thenReturn(statement)
+      connection
     }
   })
 
-  test("subsequent calls use the same connection")({
+  test("subsequent calls use the same connection from factory")({
     new EasyJDBC {
-      withConnection(c => {})
-      withConnection(c => {})
+      withConnection(c => {
+        sqlQuery("select 1 from dual") { rs => }
+        sqlQuery("select 1 from dual") { rs => }
+      })
     }
     assert(count === 1)
   })
 
   test("separate calls use different connection from factory")({
+    var c1, c2: Connection = null
+    new EasyJDBC {
+      assert(count === 0)
+      withConnection(c => { c1 = c })
+
+      assert(count === 1)
+      withConnection(c => { c2 = c })
+
+      assert(count === 2)
+      assert(c1 != c2)
+    }
+  })
+
+  test("separate objects use different connection from factory")({
     new Object with EasyJDBC {
       withConnection(c => {})
     }
